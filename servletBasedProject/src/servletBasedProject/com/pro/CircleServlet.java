9353c57a53a673c892eef8ff33893021f1354bc9
hirefire <iammilan26@gmail.com>
2020-03-07 02:58:00 -0600

    Circle Servlet class added.

---
+++ servletBasedProject/src/servletBasedProject/com/pro/CircleServlet.java
@@ -0,0 +1,13 @@
+package servletBasedProject.com.pro;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CircleServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+	{
+		System.out.println("Response defined");
+	}
+
+}
